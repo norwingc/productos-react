c9179c1a915b239be2055525a7f75ff1ef629f62
Norwin Guerrero <norwingcruz@gmail.com>
2025-02-27 23:14:16 -0500

    refactor routing by replacing react-router-dom with react-router and implement new layout structure

---
+++ client/src/layouts/Layout.tsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router";
+
+export default function Layout() {
+    return <>
+        <h1>Layout</h1>
+        <Outlet />
+    </>;
+}
---
+++ client/src/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "./layouts/Layout";
+import Products from "./pages/Products";
+import NewProduct from "./pages/NewProduct";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Products />,
+            },
+            {
+                path: "products/create",
+                element: <NewProduct />,
+            },
+        ],
+    },
+]);
