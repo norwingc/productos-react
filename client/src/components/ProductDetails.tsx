b6ddb1d2e47274d8439be923e65e27af3e26c06d
Norwin Guerreo <norwingcruz@gmail.com>
2025-05-21 00:37:04 -0500

    Agregar ruta para editar productos y enlace en los detalles del producto para facilitar la edición.

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,5 +1,6 @@
 import { Product } from "../types";
 import { formatPrice } from "../utils";
+import { Link, useNavigate } from "react-router";
 
 export default function ProductDetails({ product }: { product: Product }) {
     return (
@@ -11,7 +12,16 @@ export default function ProductDetails({ product }: { product: Product }) {
             <td className="p-3 text-lg text-gray-800">
                 {product.available ? "Disponible" : "No disponible"}
             </td>
-            <td className="p-3 text-lg text-gray-800"></td>
+            <td className="p-3 text-lg text-gray-800">
+                <div className="flex justify-center items-center gap-2">
+                    <Link
+                        to={`/products/${product.id}/edit`}
+                        className="rounded-md bg-teal-800 p-3 text-sm font-bold text-white shadow-sm hover:bg-teal-800/80"
+                    >
+                        Editar
+                    </Link>
+                </div>
+            </td>
         </tr>
     );
 }
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Layout from "./layouts/Layout";
 import Products, { loader as productsLoader } from "./pages/Products";
 import NewProduct, { action as newProductAction } from "./pages/NewProduct";
+import EditProduct from "./pages/EditProduct";
 
 export const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
                 path: "products/create",
                 element: <NewProduct />,
                 action: newProductAction,
+            },
+            {
+                path: "products/:id/edit",
+                element: <EditProduct />,
             },
         ],
     },
